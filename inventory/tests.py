3c138c0f4f80a8d5f36681ef87c2ca34081873b4
unknown <User@DESKTOP-RQGFT5F.th.local>
2022-12-15 14:01:52 +0800

    update unit test

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+
+class InventoryTestCase(TestCase):
+    def setUp(self):
+        # Create the client
+        self.client = Client()
+
+    def test_get_APIInventorys(self):
+        # Visit the inventory list page
+        response = self.client.get('/api/inventory', follow=True)
+
+        # Assert whether the response is equal to 200 or not
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_inventory(self):
+        # Visit the inventory list page
+        response = self.client.get('/inventory')
+
+        # Assert whether the response is equal to 200 or not
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_inventory_byID(self):
+        # Visit the inventory list page
+        response = self.client.get('/inventory/1')
+
+        # Assert whether the response is equal to 200 or not
+        self.assertEqual(response.status_code, 200)
